f8299f1cfa4f283004eddc4351a24b22887ce9d5
test <test@test.com>
2021-12-31 21:53:56 +0530

    Login form component setup.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Navbar from './components/Layout/Navbar';
 import Footer from './components/Layout/Footer';
 import Home from './components/Home/Home'
+import Login from './components/Login/Login'
 
 function App() {
   return (
    <React.Fragment>
      <Navbar />
-     <Home />
+     <Login />
      <Footer />
    </React.Fragment>
   );
