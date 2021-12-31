865022c925f2eefc9e825922a80ab1c9dbf8b2c6
test <test@test.com>
2021-12-31 20:49:27 +0530

    project cleanup.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World.</h1>
+    <button className='btn btn-danger btn -sm'>Save</button>
+    </div>
+  );
+}
+
+export default App;
